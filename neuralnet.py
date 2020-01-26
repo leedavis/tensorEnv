4efb34e3f022a5d636f5a138404bb32ce806b479
leedavis <leescottdavis@gmail.com>
2020-01-26 12:17:26 +1000

    Image classification

--- neuralnet.py
+++ neuralnet.py
@@ -21,8 +21,20 @@ model = keras.Sequential([keras.layers.Flatten(input_shape=(28,28)),
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 model.fit(train_images, train_labels, epochs=5)
 
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print("Tested Acc: ", test_acc)
+#test_loss, test_acc = model.evaluate(test_images, test_labels)
+#print("Tested Acc: ", test_acc)
+
+prediction = model.predict(test_images)
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Prediction " + class_names[np.argmax(prediction[i])])
+    plt.show()
+
+
+
 
 
 
